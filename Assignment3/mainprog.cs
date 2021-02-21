42e5734178fe310a7a8282e7980c136e563abf96
Christopher Nguyen <chrispy.nguyen@hotmail.com>
2021-02-21 16:30:18 +0100

    Add project files.

---
+++ Assignment3/mainprog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using SpaceSim;
+
+class Astronomy 
+{
+    public static void Main()
+    {
+        List<SpaceObject> solarSystem = new List<SpaceObject>
+        {
+        new Star("Sun"),
+        new Planet("Mercury"),
+        new Planet("Venus"),
+        new Planet("Terra"),
+        new Moon("The Moon")
+        };
+
+          foreach(SpaceObject obj in solarSystem) {
+             obj.Draw();
+        }
+        Console.ReadLine();
+    }
+}
